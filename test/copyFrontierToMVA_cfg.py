9d5fbe6339a793af0255394bf45321a569c79790
pvmulder <pvmulder@cern.ch>
2015-07-07 15:13:04 +0200

    updated with latest records

--- test/copyFrontierToMVA_cfg.py
+++ test/copyFrontierToMVA_cfg.py
@@ -8,22 +8,35 @@ process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'START53_V25::All'
+process.GlobalTag.globaltag = 'MCRUN2_74_V9::All'
 
 #process.prefer("BTauMVAJetTagComputerRecord")
 
 process.jetTagMVATrainerSave = cms.EDAnalyzer("JetTagMVATrainerFileSave",
 	trained = cms.untracked.bool(False),
 	CombinedMVA = cms.string('CombinedMVA.mva'), 
+
 	CombinedSVRecoVertex = cms.string('CombinedSVRecoVertex.mva'), 
 	CombinedSVPseudoVertex = cms.string('CombinedSVPseudoVertex.mva'), 
 	CombinedSVNoVertex = cms.string('CombinedSVNoVertex.mva'), 
-	CombinedSVMVARecoVertex = cms.string('CombinedSVMVARecoVertex.mva'), 
-	CombinedSVMVAPseudoVertex = cms.string('CombinedSVMVAPseudoVertex.mva'), 
-	CombinedSVMVANoVertex = cms.string('CombinedSVMVANoVertex.mva'),
+
 	GhostTrackRecoVertex = cms.string('GhostTrackRecoVertex.mva'),
-        GhostTrackPseudoVertex = cms.string('GhostTrackPseudoVertex.mva'),
-        GhostTrackNoVertex = cms.string('GhostTrackNoVertex.mva')
+  GhostTrackPseudoVertex = cms.string('GhostTrackPseudoVertex.mva'),
+  GhostTrackNoVertex = cms.string('GhostTrackNoVertex.mva'),
+
+	CombinedSVIVFV2RecoVertex = cms.string('CombinedSVIVFV2RecoVertex.mva'), 
+	CombinedSVIVFV2PseudoVertex = cms.string('CombinedSVIVFV2PseudoVertex.mva'), 
+	CombinedSVIVFV2NoVertex = cms.string('CombinedSVIVFV2NoVertex.mva'),
+	
+	CombinedSVRecoVertexNoSoftLepton = cms.string('CombinedRecoVertexNoSoftLepton.mva'), 
+	CombinedSVPseudoVertexNoSoftLepton = cms.string('CombinedPseudoVertexNoSoftLepton.mva'), 
+	CombinedSVNoVertexNoSoftLepton = cms.string('CombinedNoVertexNoSoftLepton.mva'),
+	CombinedSVRecoVertexSoftMuon = cms.string('CombinedRecoVertexSoftMuon.mva'),  
+	CombinedSVPseudoVertexSoftMuon = cms.string('CombinedPseudoVertexSoftMuon.mva'), 
+	CombinedSVNoVertexSoftMuon = cms.string('CombinedNoVertexSoftMuon.mva'),
+	CombinedSVRecoVertexSoftElectron = cms.string('CombinedRecoVertexSoftElectron.mva'), 
+	CombinedSVPseudoVertexSoftElectron = cms.string('CombinedPseudoVertexSoftElectron.mva'), 
+	CombinedSVNoVertexSoftElectron = cms.string('CombinedNoVertexSoftElectron.mva'), 
 )
 
 process.outpath = cms.EndPath(process.jetTagMVATrainerSave)
--- test/copyMVAToSQLite_cfg.py
+++ test/copyMVAToSQLite_cfg.py
@@ -8,9 +8,29 @@ process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.calib = cms.ESSource("BTauGenericMVAJetTagComputerFileSource",
-	CombinedSVV2RecoVertex = cms.string('CombinedSVRecoVertex.mva'), 
-	CombinedSVV2PseudoVertex = cms.string('CombinedSVPseudoVertex.mva'), 
-	CombinedSVV2NoVertex = cms.string('CombinedSVNoVertex.mva'), 
+  CombinedSVRecoVertex = cms.string('ExtractedRecords/CombinedSVRecoVertex.mva'), 
+	CombinedSVPseudoVertex = cms.string('ExtractedRecords/CombinedSVPseudoVertex.mva'), 
+	CombinedSVNoVertex = cms.string('ExtractedRecords/CombinedSVNoVertex.mva'), 
+	
+	GhostTrackRecoVertex = cms.string('ExtractedRecords/GhostTrackRecoVertex.mva'),
+  GhostTrackPseudoVertex = cms.string('ExtractedRecords/GhostTrackPseudoVertex.mva'),
+  GhostTrackNoVertex = cms.string('ExtractedRecords/GhostTrackNoVertex.mva'),
+	
+	CombinedMVA = cms.string('Kevin_cMVA/cMVA.mva'),
+	
+	CombinedSVIVFV2RecoVertex = cms.string('ExtractedRecords/CombinedSVIVFV2RecoVertex.mva'), 
+	CombinedSVIVFV2PseudoVertex = cms.string('ExtractedRecords/CombinedSVIVFV2PseudoVertex.mva'), 
+	CombinedSVIVFV2NoVertex = cms.string('ExtractedRecords/CombinedSVIVFV2NoVertex.mva'),
+	
+	CombinedSVRecoVertexNoSoftLepton = cms.string('ExtractedRecords/CombinedRecoVertexNoSoftLepton.mva'), 
+	CombinedSVPseudoVertexNoSoftLepton = cms.string('ExtractedRecords/CombinedPseudoVertexNoSoftLepton.mva'), 
+	CombinedSVNoVertexNoSoftLepton = cms.string('ExtractedRecords/CombinedNoVertexNoSoftLepton.mva'),
+	CombinedSVRecoVertexSoftMuon = cms.string('ExtractedRecords/CombinedRecoVertexSoftMuon.mva'),  
+	CombinedSVPseudoVertexSoftMuon = cms.string('ExtractedRecords/CombinedPseudoVertexSoftMuon.mva'), 
+	CombinedSVNoVertexSoftMuon = cms.string('ExtractedRecords/CombinedNoVertexSoftMuon.mva'),
+	CombinedSVRecoVertexSoftElectron = cms.string('ExtractedRecords/CombinedRecoVertexSoftElectron.mva'), 
+	CombinedSVPseudoVertexSoftElectron = cms.string('ExtractedRecords/CombinedPseudoVertexSoftElectron.mva'), 
+	CombinedSVNoVertexSoftElectron = cms.string('ExtractedRecords/CombinedNoVertexSoftElectron.mva'), 
 )
 
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
@@ -27,10 +47,31 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
 process.jetTagMVATrainerSave = cms.EDAnalyzer("JetTagMVATrainerSave",
 	toPut = cms.vstring(),
 	toCopy = cms.vstring(
-		'CombinedSVV2RecoVertex', 
-		'CombinedSVV2PseudoVertex', 
-		'CombinedSVV2NoVertex', 
-	)
+	  'CombinedSVRecoVertex', 
+	  'CombinedSVPseudoVertex', 
+	  'CombinedSVNoVertex', 
+		
+	  'GhostTrackRecoVertex',
+		'GhostTrackPseudoVertex',
+		'GhostTrackNoVertex',
+		
+		'CombinedMVA',
+		
+		'CombinedSVIVFV2RecoVertex',
+		'CombinedSVIVFV2PseudoVertex',
+		'CombinedSVIVFV2NoVertex',
+		
+		'CombinedSVRecoVertexNoSoftLepton',
+		'CombinedSVPseudoVertexNoSoftLepton',
+		'CombinedSVNoVertexNoSoftLepton',
+		'CombinedSVRecoVertexSoftMuon',
+		'CombinedSVPseudoVertexSoftMuon',
+		'CombinedSVNoVertexSoftMuon',
+		'CombinedSVRecoVertexSoftElectron',
+		'CombinedSVPseudoVertexSoftElectron',
+		'CombinedSVNoVertexSoftElectron',
+		
+		)
 )
 
 process.outpath = cms.EndPath(process.jetTagMVATrainerSave)
